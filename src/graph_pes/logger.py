d42c3e128a02dbca316d1f3aedab0facfa33df17
John Gardner <gardner.john97+dphil@gmail.com>
2024-07-19 10:13:57 +0100

    handle logs for distributed case

--- src/graph_pes/logger.py
+++ src/graph_pes/logger.py
@@ -4,11 +4,16 @@ import logging
 import sys
 from pathlib import Path
 
+from graph_pes.util import is_global_rank_zero
+
 __all__ = ["logger", "log_to_file", "set_level"]
 
 
 class MultiLineFormatter(logging.Formatter):
     """Detect multi-line logs and adds newlines."""
+
+    def __init__(self):
+        super().__init__("[%(name)s %(levelname)s]: %(message)s")
 
     def format(self, record):
         record.msg = str(record.msg).strip()
@@ -19,17 +24,17 @@ class MultiLineFormatter(logging.Formatter):
 
 # create the graph-pes logger
 logger = logging.getLogger(name="graph-pes")
-
-# set the formatter
-_formatter = MultiLineFormatter("[%(name)s %(levelname)s]: %(message)s")
-
-# log to stdout
-_handler = logging.StreamHandler(stream=sys.stdout)
-_handler.setFormatter(_formatter)
-logger.addHandler(_handler)
-
 # capture all logs
 logger.setLevel(logging.DEBUG)
+
+# set the formatter
+
+# log to stdout on global rank 0
+if is_global_rank_zero():
+    _handler = logging.StreamHandler(stream=sys.stdout)
+    _handler.setFormatter(MultiLineFormatter())
+    logger.addHandler(_handler)
+
 # but only show INFO and above in stdout (by default)
 _handler.setLevel(logging.INFO)
 
@@ -42,7 +47,7 @@ def log_to_file(file: str | Path):
 
     handler = logging.FileHandler(file, mode="a")
     handler.setLevel(logging.DEBUG)
-    handler.setFormatter(_formatter)
+    handler.setFormatter(MultiLineFormatter())
 
     logger.addHandler(handler)
     logger.info(f"Logging to {file}")
