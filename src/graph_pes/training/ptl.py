8c894ccef817ca5afe3de0fc5b5172a8bda8c7eb
John Gardner <gardner.john97+dphil@gmail.com>
2024-08-23 11:41:19 +0100

    logged progress bar

--- src/graph_pes/training/ptl.py
+++ src/graph_pes/training/ptl.py
@@ -14,7 +14,7 @@ from graph_pes.graphs.operations import number_of_structures
 from graph_pes.logger import logger
 from graph_pes.training.loss import RMSE, Loss, PerAtomEnergyLoss, TotalLoss
 from graph_pes.training.opt import LRScheduler, Optimizer
-from graph_pes.training.ptl_utils import ModelTimer
+from graph_pes.training.ptl_utils import LoggedProgressBar, ModelTimer
 from graph_pes.training.util import log_model_info
 from pytorch_lightning.callbacks import (
     EarlyStopping,
@@ -253,6 +253,7 @@ def create_trainer(
     valid_available: bool = False,
     logger: Logger | None = None,
     output_dir: Path | None = None,
+    progress: Literal["rich", "logged"] = "rich",
 ) -> pl.Trainer:
     # create the default callbacks
     callbacks: dict[str, pl.Callback] = dict(
@@ -265,7 +266,10 @@ def create_trainer(
             save_top_k=1,
             save_weights_only=True,
         ),
-        progress_bar=RichProgressBar(),
+        progress_bar={
+            "rich": RichProgressBar(),
+            "logged": LoggedProgressBar(),
+        }[progress],
         timer=ModelTimer(),
     )
     if early_stopping_patience is not None:
